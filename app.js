1161ed6d78b4faf252e96749ae4288956a154d02
9tau <86876756+9tau@users.noreply.github.com>
2021-12-22 21:22:50 +0900

    4.5 Saving Username

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
@@ -15,9 +14,11 @@ function OnLoignSubmit() {
 
 //loginForm id에 해당 인자를 add하거나 remove 한다\
 //`Hello ${username}` = "Hello " + username;
+//localstorage는 브라우저에 변수을 선언하고 인자를 저장, 제거할 수 있다
 function handleLinkClick(event) {
     event.preventDefault();
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerText= `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
