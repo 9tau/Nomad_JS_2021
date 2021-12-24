56a1fa1b1c98c549189c0605fe93069852ee8db0
9tau <86876756+9tau@users.noreply.github.com>
2021-12-24 14:43:22 +0900

    5.2 PadStart
    PadStart, PadEnd 함수에 대해 배움

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,13 @@
 const clock = document.querySelector("h2#clock");
 
 
+//padstart(a,b) 함수는 앞에 a자리수 만큼 b로 채운다
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}: ${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2,0);
+    const minutes = String(date.getMinutes()).padStart(2,0);
+    const seconds = String(date.getSeconds()).padStart(2,0);
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
