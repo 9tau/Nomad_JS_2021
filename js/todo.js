0da7b2c65f998d350b9a9d6905da4cd76e80cfd6
9tau <86876756+9tau@users.noreply.github.com>
2021-12-28 22:43:37 +0900

    7.5 Loading ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos)); //JSON.stringify는 배열에 저장할 때 string 형태로 저장한다
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //JSON.stringify는 배열에 저장할 때 string 형태로 저장한다
 }
 
 function deleteToDo (event) {
@@ -29,10 +31,20 @@ function paintToDo(newToDo) {
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value; //newToDo 변수에 새로운 값을 저장
-    toDos.push(newToDo);
+    toDos.push(newToDo); //toDos에 변수값 저장
     console.log(toDoInput.value);
     paintToDo(newToDo);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);  //savedToDos 전체 변수값을 각각의 object로 변환
+    parsedToDos.forEach((item) => console.log("this is the turn of", item)); //array 각각의 데이터 마다 sayHello 함수 발동
+    // => arrow function 사용
+    toDos = parsedToDos; //새로고침 시 toDos에 저장된 값이 초기화되지 않도록 저장
+    parsedToDos.forEach(paintToDo);
+}
